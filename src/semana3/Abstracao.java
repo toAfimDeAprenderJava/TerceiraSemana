457083f365acc9e59e7557ac57f81fc41aecef3f
Paulo <p.felipe@msn.com>
2018-10-23 20:53:24 -0300

    inicial commit

---
+++ src/semana3/Abstracao.java
@@ -0,0 +1,14 @@
+package semana3;
+
+/**
+ * Abstraia dois objeto da vida real em um objeto Java
+ * coloque ao menos 3 métodos e 3 atributos 
+ * encapsule todos os metodos e atributos
+ *
+ */
+public class Abstracao {
+	public static void main(String[] args) {
+		
+	
+	}
+}
---
+++ src/semana3/HierarquiaPolimorfismo.java
@@ -0,0 +1,22 @@
+package semana3;
+
+/**
+ *	Criar uma estrutura hierárquica que contenha as seguintes classes:
+ * 	Veiculo (classe abstracta), Bicicleta e Automóvel.
+ * 	Os métodos da classe Veiculo são todos abstractos e possuem a seguinte assinatura:
+ * qntdRodas(int)
+ * qntdPortas(int)
+ * qntdBanco(int)
+ * deslocar(double)
+ * frear()
+ * String marca
+ * Estes métodos são implementados nas subclasses Automóvel e Bicicleta.
+ * Acrescentar na classe Automóvel o método mudarOleo()
+ *  encapsule todos os metodos e atributos
+ */
+public class HierarquiaPolimorfismo {
+	public static void main(String[] args) {
+		
+	
+	}
+}
