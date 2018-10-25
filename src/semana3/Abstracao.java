4f0ab786decbb207f6e41d676bb2404ebb508537
brvnv <bruno.rdx@metalfan.com>
2018-10-25 10:46:21 -0300

    Mais métodos, código mais relevante, removido desligamento automático do toca disco quando agulha levanta (certamente estava distraído quando programei assim)

--- src/semana3/Abstracao.java
+++ src/semana3/Abstracao.java
@@ -5,7 +5,6 @@ package semana3;
  * coloque ao menos 3 mÃ©todos e 3 atributos 
  * encapsule todos os metodos e atributos
  *
- *Naturalmente os métodos não funcionam aqui, da forma que eu pensei classes como Automatico extends TocaDisco, e isso é para o próximo exercício.
  */
 
 public class TocaDisco {
@@ -13,32 +12,63 @@ public class TocaDisco {
 	private String marca;
 	private String modelo;
 	private boolean suporta45rpm;
-	
-	private static boolean onoff() {
-		if(onoff==false) {
-			return true;
-			System.out.println("Seu " + marca + " " + modelo + " está agora ligado.");
-		}
-		return false;
-		if(emFuncionamento==true) System.out.println("O pitch da música diminui conforme o braço equipado com sua " + Agulha.getTipo() + " repousa e o toca disco desliga.");
-		System.out.println("Seu " + marca + " " + modelo + " está agora desligado.");
+	//getters e setters dos atributos
+	private String getMarca() {
+		return marca;
 	}
 	
-	private static void insereDisco(int polegadas) {
-		if((polegadas==7) && (suporta45rpm==false)) {
-			System.out.println("Disco não pode ser tocado.");
-			emFuncionamento=false;
-		}
-		if(polegadas!=12) {
-			System.out.println("Insira apenas discos.");
-			emFuncionamento=false;
-		}
-		emFuncionamento=true;
-		System.out.println("O braço repousa sobre o prato");
+	private String getModelo() {
+		return modelo;
+	}
+
+	private boolean getSuporta45rpm() {
+		return suporta45rpm;
 	}
 	
 	private boolean getEmFuncionamento() {
 		return emFuncionamento;
+	}
+	
+	private void setMarca(String marca) {
+		this.marca=marca;
+	}
+	
+	private void setModelo(String modelo) {
+		this.modelo=modelo;
+	}
+	
+	private void setSuporta45rpm(boolean suporta45rpm) {
+		this.suporta45rpm=suporta45rpm;
+	}
+	
+	private boolean setEmFuncionamento(boolean emFuncionamento) {
+		if(this.emFuncionamento==emFuncionamento) break; //pula se nada mudou (evitar erros se algo na implementação do onoff é alterado, por padrão isso não aconteceria)
+		this.emFuncionamento=emFuncionamento;
+	}
+	
+
+	//métodos
+	private static void onoff() {
+		if(getEmFuncionamento()==false) {
+			setEmFuncionamento (true);
+		} else{
+			Agulha.levanta();
+		setEmFuncionamento (false);
+		}
+		System.out.println("Seu " + getMarca() + " " + getModelo() + " está agora " + (getEmFuncionamento() ? "ligado." : "desligado."))
+	}
+	
+	private static void insereDisco(int polegadas) {
+		if((polegadas==7) && (getSuporta45rpm==false)) {
+			System.out.println("Disco não pode ser tocado.");
+			setEmFuncionamento(false);
+		}
+		if(polegadas!=12) {
+			System.out.println("Insira apenas discos.");
+			setEmFuncionamento(false);
+		}
+		setEmFuncionamento(true);
+		System.out.println("O braço repousa sobre o prato");
 	}
 }
 
@@ -46,22 +76,32 @@ public class Agulha {
 	private String tipo;
 	private int peso;
 	private int precisao;
-	boolean estaLevantada;
+	boolean estaLevantada=true;
+
+private String getTipo() {
+	return tipo;
+}
 
 private boolean getEstaLevantada(){
 	return estaLevantada;
 }
 
+private void setTipo(String tipo) {
+	this.tipo=tipo;
+}
+
+private boolean setEstaLevantada(boolean estaLevantada) {
+	this.estaLevantada=estaLevantada;
+}
+
 private static void levanta() {
-	if(TocaDisco.getEmFuncionamento==false) System.out.println("Sua caríssima agulha de tipo " + tipo + " se perde em meio aos fios dos eletrodomésticos. Boa sorte");
-	if(estaLevantada==true) System.out.println("A agulha aponta para o teto e reproduz a melodia que consegue capturar de uma lâmpada.");
+	if(TocaDisco.getEmFuncionamento()==false) System.out.println("Sua caríssima agulha de tipo " + getTipo() + " se perde em meio aos fios dos eletrodomésticos. Boa sorte");
+	if(getEstaLevantada()==true) System.out.println("A agulha aponta para o teto e reproduz a melodia que consegue capturar de uma lâmpada.");
 	System.out.println("O pitch diminui até finalmente chegar em 0. A música para");
-	TocaDisco.emFuncionamento=false;
-	estaLevantada=false;
+	setEstaLevantada(true);
 }
 
 private static void abaixa() {
-	if(TocaDisco.getEmFuncionamento()==false) System.out.println("A agulha se abaixa. Nada acontece");
 	if(agulha.getEstaLevantada()==false) System.out.println("Você se pergunta de onde a agulha está tirando músicas do Slayer");
 	System.out.println("A agulha retorna ao seu habitat natural, junto com a música.");
 	tocaDisco.emFuncionamento=true;
