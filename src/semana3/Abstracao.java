8a8a2d62114a037a685d1b59edab4d478bd058ef
DeyseSouza <deyseluciasp@hotmail.com>
2018-10-28 18:44:29 -0300

    Exercicio terceira semana

--- src/semana3/Abstracao.java
+++ src/semana3/Abstracao.java
@@ -1,14 +1,56 @@
 package semana3;
 
 /**
- * Abstraia dois objeto da vida real em um objeto Java
- * coloque ao menos 3 métodos e 3 atributos 
- * encapsule todos os metodos e atributos
+ * Abstraia dois objeto da vida real em um objeto Java coloque ao menos 3
+ * métodos e 3 atributos encapsule todos os metodos e atributos
  *
  */
 public class Abstracao {
-	public static void main(String[] args) {
-		
-	
+
+	public abstract class ControleRemoto {
+
+		private boolean ligada;;
+		private int mudarcanal;
+		private String volume;
+		private boolean desligartv;
+
+		public void ligar() {
+			ligada = true;
+		}
+
+		public boolean getliga() {
+			return ligada;
+
+		}
+
+		public void mudarcanal(int canal) {
+			if (canal < 1 || canal > 60)
+				throw new RuntimeException();
+			this.mudarcanal = canal;
+		}
+
+		public int getmudarcanal() {
+
+			return mudarcanal;
+
+		}
+
+		public void volume(double quantidade) {
+			this.volume = this.volume += quantidade;
+		}
+
+		public String getvolume() {
+
+			return volume;
+		}
+
+		public void desligartv() {
+			desligartv = true;
+		}
+
+		public boolean getdesligartv() {
+			return desligartv;
+		}
+
 	}
 }
