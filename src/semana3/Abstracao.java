060ea17d1a375c372b859bd67b7da703bea0d020
Pedro Nascimento <pedrovieira10@icloud.com>
2018-10-30 11:00:56 -0200

    Conclusão terceira semana

--- src/semana3/Abstracao.java
+++ src/semana3/Abstracao.java
@@ -7,8 +7,61 @@ package semana3;
  *
  */
 public class Abstracao {
-	public static void main(String[] args) {
-		
-	
+	private String cor = "Vermelho";
+	private String nome = "Ferrari";
+	private double preco  = 200.000;
+
+	public String getCor() {
+		return cor;
 	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public void imprimir(){
+		System.out.println(nome);
+		System.out.println(preco);
+		System.out.println(cor);
+	}
+
+	public void modificarNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void modificarPreco(double preco){
+		this.preco = preco;
+	}
+
+	public static void main(String[] args) {
+
+		Abstracao carro = new Abstracao();
+
+		carro.imprimir();
+		carro.modificarNome("Uno");
+		carro.modificarPreco(100.0);
+
+		System.out.println("===============================");
+
+		carro.imprimir();
+
+
+	}
+
 }
